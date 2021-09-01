037aed610db46927df073b5c5b83c7d15d86d462
Jeffrey Paarhuis <jpaarhuis@home.nl>
2021-09-01 19:10:33 +0200

    Revert "roll-me-back"
    This reverts commit 6b8e91b2d1a1acfa8790c660a22f0c04dcf3b87d.

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Reflection;
-using app.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using Serilog.Sinks.Elasticsearch;
 
 namespace app
 {
@@ -15,52 +7,13 @@ namespace app
     {
         public static void Main(string[] args)
         {
-            ConfigureLogging();
-            
             CreateHost(args);
         }
 
         private static void CreateHost(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                db.Database.Migrate();
-            }
-
             host.Run();
-        }
-
-        private static void ConfigureLogging()
-        {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var configuration = new ConfigurationBuilder()
-              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-              .AddJsonFile(
-                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-                optional: true)
-              .Build();
-
-            Log.Logger = new LoggerConfiguration()
-              .Enrich.FromLogContext()
-              .Enrich.WithMachineName()
-              .WriteTo.Debug()
-              .WriteTo.Console()
-              .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
-              .Enrich.WithProperty("Environment", environment)
-              .ReadFrom.Configuration(configuration)
-              .CreateLogger();
-        }
-
-        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
-        {
-            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-            {
-                AutoRegisterTemplate = true,
-                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-            };
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -68,7 +21,6 @@ namespace app
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                })
-                .UseSerilog();
+                });
     }
 }
