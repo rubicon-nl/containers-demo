c7a58a9090549b6cd49cca7d0b645e473ed874d4
Jeffrey Paarhuis <jeffrey@paarhu.is>
2021-09-08 18:51:48 +0200

    CICD

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,5 +1,13 @@
+using System;
+using System.Reflection;
+using app.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace app
 {
@@ -7,13 +15,52 @@ namespace app
     {
         public static void Main(string[] args)
         {
+            ConfigureLogging();
+
             CreateHost(args);
         }
 
         private static void CreateHost(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
             host.Run();
+        }
+
+        private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile(
+                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                optional: true)
+              .Build();
+
+            Log.Logger = new LoggerConfiguration()
+              .Enrich.FromLogContext()
+              .Enrich.WithMachineName()
+              .WriteTo.Debug()
+              .WriteTo.Console()
+              .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+              .Enrich.WithProperty("Environment", environment)
+              .ReadFrom.Configuration(configuration)
+              .CreateLogger();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +68,7 @@ namespace app
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
